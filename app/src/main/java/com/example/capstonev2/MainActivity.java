b85b635368cb6af7b7f0922d8fa4f764c6c82e84
kzheng01 <73297311+kzheng01@users.noreply.github.com>
2020-12-08 11:55:20 -0500

    Added Listen Button, recombining Wyatts and my project

--- app/src/main/java/com/example/capstonev2/MainActivity.java
+++ app/src/main/java/com/example/capstonev2/MainActivity.java
@@ -2,15 +2,28 @@ package com.example.capstonev2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.speech.RecognizerIntent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    public static final Integer RecordAudioRequestCode = 1;
+    private spee speechRecognizer;
+    private EditText editText;
+    private ImageView micButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,14 +34,45 @@ public class MainActivity extends AppCompatActivity
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null)
         {
-            final Toast no_bluetooth_detected = Toast.makeText(getApplicationContext(),"No bluetooth detected", Toast.LENGTH_SHORT);
+            @SuppressLint("ShowToast") final Toast no_bluetooth_detected = Toast.makeText(getApplicationContext(),"No bluetooth detected", Toast.LENGTH_SHORT);
             finish();
         }
-            }
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
+        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED);
+        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+        this.registerReceiver(mReceiver, filter);
+    }
 
     public final void bluetooth( View view) {
         Intent intentOpenBluetoothSettings = new Intent();
         intentOpenBluetoothSettings.setAction("android.settings.BLUETOOTH_SETTINGS");
         this.startActivity(intentOpenBluetoothSettings);
     }
+
+    public void setSpeechRecognizer(spee speechRecognizer) {
+        this.speechRecognizer = speechRecognizer;
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver()
+    {
+        TextView Connectivity;
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+            if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+                @SuppressLint("ShowToast") final Toast no_bluetooth_detected = Toast.makeText(getApplicationContext(),"Bluetooth Device is now Connected", Toast.LENGTH_SHORT);
+                Connectivity.setText("Disconnected");
+                //Device is now connected
+            }
+            else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+                @SuppressLint("ShowToast") final Toast no_bluetooth_detected = Toast.makeText(getApplicationContext(),"Bluetooth Device has been Disconnected", Toast.LENGTH_SHORT);
+                Connectivity.setText("Disconnected");
+                //Device has disconnected
+            }
+        }
+    };
 }
---
+++ app/src/main/java/com/example/capstonev2/spee.java
@@ -0,0 +1,4 @@
+package com.example.capstonev2;
+
+public class spee {
+}
